0c16d4acf368f75ab491314744e8f96eca92984e
Marcel-ICMC <marcelotoboni@gmail.com>
2026-03-29 20:33:17 -0300

    Adding test_local_adapter

---
+++ tests/unit/test_local_adapter.py
@@ -0,0 +1,28 @@
+# tests/unit/test_local_adapter.py
+import pytest
+
+from adapters import LocalAdapter
+
+
+@pytest.mark.asyncio
+async def test_correct_letter_right_position() -> None:
+    adapter = LocalAdapter(answer="carro")
+    await adapter.input_submit_guess("carro")
+    result = await adapter.fetch_row_result(0)
+    assert result == ["right", "right", "right", "right", "right"]
+
+
+@pytest.mark.asyncio
+async def test_correct_letter_wrong_position() -> None:
+    adapter = LocalAdapter(answer="carro")
+    await adapter.input_submit_guess("rroca")  # r and a are in wrong positions
+    result = await adapter.fetch_row_result(0)
+    assert result[0] == "place"
+
+
+@pytest.mark.asyncio
+async def test_wrong_letter() -> None:
+    adapter = LocalAdapter(answer="carro")
+    await adapter.input_submit_guess("bbbbb")
+    result = await adapter.fetch_row_result(0)
+    assert all(r == "wrong" for r in result)
